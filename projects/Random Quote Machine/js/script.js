7d4e8f25753b8f53f3c6e317d7d9569b37364f4e
manolol <manoel.felipe.live@gmail.com>
2018-03-26 07:27:20 -0300

    test3

--- projects/Random Quote Machine/js/script.js
+++ projects/Random Quote Machine/js/script.js
@@ -14,7 +14,7 @@ let getQuote = () => {
     }
 
     $.ajax({
-        url: "http://api.forismatic.com/api/1.0/",
+        url: "https://api.forismatic.com/api/1.0/",
         jsonp: "jsonp",
         dataType: "jsonp",
         data: {

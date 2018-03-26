e517bc9cc0f222909a7e2e7e733124e5c4b7301b
manolol <manoel.felipe.live@gmail.com>
2018-03-25 22:42:31 -0300

    random quote machine finished

--- projects/Random Quote Machine/js/script.js
+++ projects/Random Quote Machine/js/script.js
@@ -1,10 +1,47 @@
-changeQuote = () => {
+let getQuote = () => {
+
+    let text, author;
+
     let randomColor = "#" + Math.floor(Math.random() * 999999);
     console.log("background color: " + randomColor);
     document.getElementById('body').style.backgroundColor = randomColor;
     let buttons = document.getElementsByClassName('button');
-    document.getElementById('quote').style.color = randomColor;
+    document.getElementById('text').style.color = randomColor;
+    document.getElementById('author').style.color = randomColor;
+    document.getElementById('quote-icon').style.color = randomColor;
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].style.backgroundColor = randomColor;
     }
-}
+
+    $.ajax({
+        url: "http://api.forismatic.com/api/1.0/",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        data: {
+            method: "getQuote",
+            lang: "en",
+            format: "jsonp"
+            
+        },
+        
+        success: function(response)   {
+          
+          text = response.quoteText;
+          author = response.quoteAuthor;
+          $("#text").text(text);
+          
+          if (author)   {
+            $("#author").text("- " + author);
+          } else  {
+            $("#author").text(" ~ Unknown");
+          }
+        }
+    });
+
+  }
+
+$(document).ready(function() {
+    getQuote();
+    $("#new-quote").click(getQuote);
+});
+

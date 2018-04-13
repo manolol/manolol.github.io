5bd13dac0d5ea2e1536b0951931f74bf673b713a
unknown <manoel.felipe.live@gmail.com>
2018-04-12 22:40:32 -0300

    Weather App created

---
+++ projects/Weather App/README.md
@@ -0,0 +1,16 @@
+https://codepen.io/freeCodeCamp/full/bELRjV
+https://medium.com/@merakireal/fcc-local-weather-app-with-html5-geolocation-and-fcc-weather-api-e98c18b4b73d
+https://fonts.google.com/
+https://fcc-weather-api.glitch.me/
+
+--To do--
+-I can see the weather in my current location.
+-I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+-I can push a button to toggle between Fahrenheit and Celsius.
+
+--Change an atribute of a HTML element:
+-jQuery
+$("id-or-class").attr("atribute-name", "atribute-value");
+-JS
+let img = document.getElementById("img-id");
+img.atribute-name="atribute-value";
---
+++ projects/Weather App/js/script.js
@@ -0,0 +1,35 @@
+// get Geolocation
+const showPosition = (position) => {
+
+    console.log("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude);
+    let api = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude;
+
+    console.log("data from: " + api);
+
+    $.getJSON(api, function(data) {
+        // Getting Weather Details
+        $("#place").html("Location: " + data.name + ", " + data.sys.country);
+        $("#temp").html("Temperature: " + data.main.temp + " C°");
+        $("#min").html("Min: " + data.main.temp_min + ", ");
+        $("#max").html("Max: " + data.main.temp_max);
+        $("#weather").html(data.weather[0].main);
+        $("#weather-description").html("(" + data.weather[0].description + ")");
+        // Getting Weather Icon
+        let weatherIcon = document.getElementById("#weather-icon");
+        weatherIcon.src = data.weather[0].icon;
+    });
+
+}
+
+const getLocation = () => {
+    if(navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        document.getElementById("#notification").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+$(document).ready(function() {
+    getLocation();
+});
